03a9551c790bcd8634f1acaac8024fe136fbcf2d
Rajiv <rajiv.agarwal@capgemini.com>
2023-05-12 11:02:15 +0000

    US-0000125: Flexcard11

---
+++ lwc/cfCOX_BillingAccountsViewAll/styleDefinition.js
@@ -0,0 +1,3 @@
+let styleDefinition =
+      {"state0element0block_element0":[{"conditions":"default","styleObject":{"class":"slds-col  condition-element slds-text-align_left slds-p-left_x-small  slds-size_12-of-12  ","style":"      \n     height:25px;    ","styleProperties":{"styles":{"label":{"color":"#0155D0","fontSize":"16px","fontFamily":"","textDecoration":""}}}}}]};
+  export default styleDefinition
---
+++ lwc/cfCox_BillingHeader3/cfCox_BillingHeader3.js
@@ -0,0 +1,61 @@
+import { FlexCardMixin } from "vlocity_cmt/flexCardMixin";
+    import {interpolateWithRegex, interpolateKeyValue, loadCssFromStaticResource } from "vlocity_cmt/flexCardUtility";
+    
+          import { LightningElement, api, track, wire } from "lwc";
+          import pubsub from "vlocity_cmt/pubsub";
+          import { getRecord } from "lightning/uiRecordApi";
+          
+          import data from "./definition";
+          
+          import styleDef from "./styleDefinition";
+              
+          export default class cfCox_BillingHeader3 extends FlexCardMixin(LightningElement){
+              @api debug;
+              @api recordId;
+              @api objectApiName;
+              
+              @track record;
+              
+              
+              pubsubEvent = [];
+              customEvent = [];
+              
+              connectedCallback() {
+                super.connectedCallback();
+                this.setStyleDefinition(styleDef);
+                data.Session = {} //reinitialize on reload
+                
+                
+                
+                this.setDefinition(data);
+ this.registerEvents();
+                this.setAttribute(
+                  "class",
+                  "card-a79DM0000005o4TYAQ"
+                );
+                this.loadCustomStylesheetAttachement("00PDM000006kwYy2AI");
+                
+                
+              }
+              
+              disconnectedCallback(){
+                super.disconnectedCallback();
+                    
+                    
+
+                  this.unregisterEvents();
+              }
+
+              registerEvents() {
+                
+              }
+
+              unregisterEvents(){
+                
+              }
+            
+              renderedCallback() {
+                super.renderedCallback();
+                
+              }
+          }
